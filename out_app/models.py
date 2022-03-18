802984e9b67f83ab7f6b116e53f0820f162699f8
ElyseRH <elyserhoran@gmail.com>
2022-03-18 10:32:14 +0000

    Add superuser for django admin

--- out_app/models.py
+++ out_app/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Ready to Send"))
 
 
-class Post(models.Model):  # can I call this page?
+class Page(models.Model):  # can I call this page?
     """
     Class for modelling posts made by creators.
     """
