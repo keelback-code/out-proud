d29be8acd6a98d352e5a5d7622652a823756fcdf
ElyseRH <elyserhoran@gmail.com>
2022-03-18 11:26:07 +0000

    Add PageAdmin class and decorator

--- out_app/admin.py
+++ out_app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Page
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Page
 
 
 @admin.register(Page)
 class PageAdmin(SummernoteModelAdmin):
-
+    """
+    Class to model Summernote attributes.
+    """
+    # prepopulated_fields = {'creator': ('User',)} logged in user? should be editable tho
+    # prepopulated_fields = {'page_code': (get_random_string,)} does this work this way?
     summernote_fields = ('text_content')
+    list_display = ('title', 'status', 'date')
+    list_filter = ('status', 'date')
